e8b8fde37ef6cf55e3c494ecf8894e3567ee9ff5
swedk <127571222+poseww@users.noreply.github.com>
2023-12-26 16:11:48 +0700

    Update script.js

--- 03/script.js
+++ 03/script.js
@@ -6,61 +6,17 @@ var navbar = document.querySelector(".navbar");
 }
 /*===== Nav Toggler =====*/
 const navMenu = document.querySelector(".menu");
-    navToggle = document.querySelector(".menu-btn");
-    if(navToggle)
-    {
-        navToggle.addEventListener("click", () =>
-        {
-            navMenu.classList.toggle("active");
-        })
-    }
-    // closing menu when link is clicked
-    const navLink = document.querySelectorAll(".nav-link");
-    function linkAction()
-    {
-        const navMenu = document.querySelector(".menu");
-        navMenu.classList.remove("active")
-    }
-    navLink.forEach(n => n.addEventListener("click", linkAction))
-    /*===== Scroll Section Active Link =====*/
-
-// const Section=document.querySelectorAll('section[id]')
-//     function scrollActive()
-//     {
-//         const scrollY = window.pageYOffset
-//         Section.forEach(current => {
-//             const sectionHeight = current.offsetHeight
-//             const sectionTop = current.offsetTop - 50;
-//             sectionId = current.getAttribute('id')
-//             if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-//                 document.querySelector('.links a[href*=' + sectionId + ']').classList.add('active')
-//             }
-//             else
-//             {
-//             document.querySelector('.links a[href*=' + sectionId + ']').classList.remove('active')
-//             }
-//         })
-//     }
-//     window.addEventListener('scroll', scrollActive)
-// /*===== Skills Animation =====*/
-// const skills_wrap = document.querySelector(".about-skills"),
-//     skills_bar = document.querySelectorAll(".progress-line");
-//     window.addEventListener("scroll", () => {
-//         skillsEffect();
-//     })
-//     // every time we scroll checking, we exceeded the about-skills or not
-//     function checkScroll(el)
-//     {
-//         //getting the top position of about-skills relative to view port, in other words we need to get
-//         // amount of pixels between about-skills and the top edge of the window.
-//         let rect = el.getBoundingClientRect();
-//         // after knowing the amount of pixels between the top edge of about skills and the top edge of window
-//         // now we will check we exceeded the bottom edge of about skills or not
-//         if(window.innerHeight >= rect.top + el.offsetHeight) return true;
-//         return false;
-//     }
-//     function skillsEffect()
-//     {
-//         if(!checkScroll(skills_wrap)) return;
-//         skills_bar.forEach((skill) => (skill.style.width = skill.dataset.progress));
-//     }
+      navToggle = document.querySelector(".menu-toggle");
+      if(navToggle) 
+      {
+          navToggle.addEventListener("click", () => {
+              navMenu.classList.toggle("active");
+          })
+      }
+      // closing menu when link is closed //
+      const navLink = document.querySelector(".nav-link");
+      function linkAction(){
+          const navMenu = document.querySelector(".menu");
+          navMenu.classList.remove("active");
+      }
+      navLink.forEach(n => n.addEventListener("click", linkAction))
